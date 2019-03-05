043d55772bff2b516ed144cafaf02c159b92d01d
janyroo <214778990@qq.com>
2019-03-05 22:48:44 +0800

    数据库使用完成

--- todolist/views.py
+++ todolist/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,redirect#render渲染,redirect重定向
-
+from .models import Todo
 # Create your views here.
 
 lis=[{'待办事项':'学习','已完成':False},{'待办事项':'吃饭','已完成':True},]
@@ -9,42 +9,51 @@ def home(request):
     global lis
     if request.method == "POST":
         if request.POST["待办事项"] == '':
-            content = {"清单": lis,'警告':'请输入内容!'}
+            content = {"清单": Todo.objects.all(),'警告':'请输入内容!'}
             return render(request,'todolist/home.html',content)
         else:
-            lis.append({"待办事项":request.POST["待办事项"],'已完成':False})
-            content={"清单":lis,'信息':'添加成功!'}
+            a_row = Todo(thing=request.POST['待办事项'])
+            a_row.save()
+            content={"清单":Todo.objects.all(),'信息':'添加成功!'}
             return render(request,'todolist/home.html',content)
     elif request.method == "GET":
-        content = {"清单": lis}
+        content = {"清单": Todo.objects.all()}
         return render(request, 'todolist/home.html', content)
+
+
 def about(request):
     return render(request,'todolist/about.html')
 
 
-def edit(request,forloop_counter):
+def edit(request,每一件事_id):
     global lis
     if request.method == "POST":
         if request.POST["已修改事项"] == '':
             return render(request, 'todolist/edit.html', {'警告': '请输入内容!'})
         else:
-            lis[int(forloop_counter) - 1]["待办事项"]=request.POST["已修改事项"]
+            a = Todo.objects.get(id=每一件事_id)
+            a.thing = request.POST["已修改事项"]
+            a.save()
             return redirect('todolist:主页')
     elif request.method == "GET":
-        content = {"待修改事项": lis[int(forloop_counter)-1]["待办事项"]}
+        a = Todo.objects.get(id=每一件事_id)
+        content = {"待修改事项": a.thing}
         return render(request,'todolist/edit.html',content)
 
 
 
-def delete(request,forloop_counter):
-    lis.pop(int(forloop_counter)-1)
+def delete(request,每一件事_id):
+    a= Todo.objects.get(id=每一件事_id)
+    a.delete()
     return redirect('todolist:主页')
 
-def cross(request,forloop_counter):
+def cross(request,每一件事_id):
     if request.POST['完成状态']=='已完成':
-        lis[int(forloop_counter) - 1]['已完成']=True
+        a = Todo.objects.get(id=每一件事_id)
+        a.done=True
     else:
-        lis[int(forloop_counter) - 1]['已完成'] = False
-
+        a = Todo.objects.get(id=每一件事_id)
+        a.done = False
+    a.save()
     return redirect('todolist:主页')
 
