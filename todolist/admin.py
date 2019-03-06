2dfd9ba43f308be05bc519ae7e74c842c0958d74
janyroo <214778990@qq.com>
2019-03-06 21:38:37 +0800

    实现Users数据连接

--- todolist/admin.py
+++ todolist/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+from .models import Todo
 
 # Register your models here.
+
+class TodoInline(admin.TabularInline):
+    model = Todo
+    can_delete = True
+    verbose_name_plural = '待办事项'
+
+class UserAdmin(BaseUserAdmin):
+    inlines = (TodoInline,)
+
+admin.site.unregister(User)
+admin.site.register(User,UserAdmin)
--- todolist/models.py
+++ todolist/models.py
@@ -1,7 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Todo(models.Model):
+
+    用户 = models.ForeignKey(User, on_delete=models.DO_NOTHING, default=1)  # 作者
     thing = models.CharField(max_length=50)
     done = models.BooleanField(default=False)
+    class Meta:
+        verbose_name_plural="待办事项"
